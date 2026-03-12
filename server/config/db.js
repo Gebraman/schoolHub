16fceaa96e893b1942a2c2188abba51115804cf3
G/medhin Endeshawe <gebremedhinendeshawe@gmail.com>
2026-03-12 15:58:29 +0300

    Add CA certificate for TiDB Cloud SSL connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
-// connect to the database using environment variables
 const mysql = require("mysql2/promise");
+const fs = require("fs");
+const path = require("path");
+
+// Path to CA certificate (Render mounts secret files at /etc/secrets/)
+const caPath = "./ca.pem";
 
 const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "schoolhub",
   password: process.env.DB_PASS || "Gebremedhin100%",
   database: process.env.DB_NAME || "schoolhub_db",
-  timezone: "Z", // Force UTC
+  port: process.env.DB_PORT || 3306,
+  timezone: "Z",
   waitForConnections: true,
+  ssl:
+    process.env.DB_SSL === "true"
+      ? {
+          ca: fs.existsSync(caPath) ? fs.readFileSync(caPath) : undefined,
+          rejectUnauthorized: true,
+        }
+      : null,
 });
 
 module.exports = db;
